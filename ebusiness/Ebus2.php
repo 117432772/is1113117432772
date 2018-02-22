dd7e9c6fbe1e45285f23dea5bb0e614047efca1f
Ciara Curtin <117432772@umail.ucc.ie>
2018-02-22 10:05:17 +0000

    Updates and added material

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -1,35 +1,44 @@
-<DOCTYPE html>
-    <html>
-        <head>
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title> Enter Payment Details</title>
+        
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    
+    <body>
+        
+        <h4>Please enter your payment details.</h4>
+        
+            <br />
             
-            <title> Enter Details</title>
+            <form method = "POST" action = "Ebus3.php">
+                
+                <label for="user_pin">
+                     PIN 
+                </label>
+                
+                <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
+                
+               <button onClick="validateDetails()"> Validate </button>
+               <br />
+                <button type="Submit" id="btnPurchase" disabled> 
+                    Proceed with Purchase 
+                </button>
+                
+            </form>
             
-            <!--jQuery-->
-            <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <br />
             
-        </head>
-         <body>
-              <h4>Please enter your payment details.</h4>
-              
-              <br/>
-              
-              <form method = "POST" action = "Ebus3.php"></form>
-              
-              <label for="user_pin"
-              placeholder="Card Pin"
-              maxlength="4"
-              
-              <button type="Submit"
-              id="btnPurchase" disabled>
-                  Proceed with Purchase
-              </button>
-              </form> 
-              <br/>
-              <buttononClick="validate details()">  Validate </button>
-              
-              <scipt type="text/javascript"
-              scr="ebus2_validator.js"></scipt>
-              
-         </body>
-    </html>
-</DOCTYPE>
+   
+        
+        <script type="text/javascript" src="ebus2_validator.js"></script>
+        
+        
+    </body>
+    
+    
+    
+</html>
---
+++ ebusiness/Ebus3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Receipt</title>
+        
+        <link rel="stylesheet" href="ebus_pages.css" type="text/css"/>
+         <link rel="stylesheet" href="/ebusiness/ebusstyle.css" type="text/css" />
+        
+    </head>
+    <body>
+        <div id="receipt">
+            <h4>Receipt of Pyament</h4>
+         
+         
+         </div>   
+    </body>
