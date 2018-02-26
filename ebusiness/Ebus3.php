90a0aaf192370df5696bf297f2a1d7117beb0a32
Ciara Curtin <117432772@umail.ucc.ie>
2018-02-26 23:10:37 +0000

    project updates

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -8,6 +8,43 @@
         
     </head>
     <body>
+          <link rel="stylesheet" href="/mystylesheet.css" type="text/css" />
+<h1 style="background-color: rgb(245, 189, 203);; /*Light Pink*/;font-size:40px;text-align:center;font-family:georgia; margin:0; padding:0">IS1113 Project</h1>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: rgb(71, 78, 93);; /*Dark Blue*/;
+}
+
+li {
+    display:inline;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111111;
+}
+</style>
+</head>
+ <body>
+     <body style="background-color:rgb(222, 57, 99);text-align:center">
+<ul>
+  <li><a href="homepage.html">Home</a></li>
+  <li><a href="cv/page1.html">CV</a></li>
+  <li><a href="interests/sports.html">Interests</a></li>
+  <li><a href="ebusiness/Ebus1.php">Products</a></li>
+</ul>
+
         <div id="receipt">
             <h4>Receipt of Pyament</h4>
          
